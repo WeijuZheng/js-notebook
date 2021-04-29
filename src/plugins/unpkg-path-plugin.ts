06804f8ff277c0f20e932f931bbe7667a9b6092f
weiju <weijuzheng1995@gmail.com>
2021-04-29 15:31:50 -0700

    added cache functionality to the plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,7 +1,12 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
 
-export const unpkgPathPlugin = () => {
+const fileCache = localForage.createInstance({
+    name: 'filecache'
+});
+
+export const unpkgPathPlugin = (inputCode: string) => {
     return {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
@@ -32,19 +37,31 @@ export const unpkgPathPlugin = () => {
                 if (args.path === 'index.js') {
                     return {
                         loader: 'jsx',
-                        contents: `
-              const message = require ('nested-test-pkg');
-              console.log(message);
-            `,
+                        contents: inputCode,
                     };
                 }
 
+                // Check to see if we have already fetched this file
+                // and if it is in the cache
+                const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+                // if it is, return it immediately
+                if (cacheResult) {
+                    return cacheResult;
+                }
+
                 const { data, request } = await axios.get(args.path);
-                return {
+
+                const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
                     contents: data,
                     resolveDir: new URL('./', request.responseURL).pathname
                 };
+
+                // store response in cache 
+                await fileCache.setItem(args.path, result);
+
+                return result;
             });
         },
     };
