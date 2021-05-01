1f23f47032b949e0a7f47a5ac21fbe3932aca219
weiju <weijuzheng1995@gmail.com>
2021-04-30 17:35:01 -0700

    added iframe element to excute code, initialize iframe with srcDoc, passing code to iframe through message event, highlighting errors

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,9 @@ import { unpkgFetchPlugin } from './plugins/unpkg-fetch-plugin';
 
 const App = () => {
     const ref = useRef<any>();
+    const iframe = useRef<any>();
     const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
+    // const [code, setCode] = useState('');
 
     // get esbuild.wasm from unpkg.com everytime the app start running
     const startService = async () => {
@@ -26,6 +27,9 @@ const App = () => {
             return;
         }
 
+        // reset the content of the iframe everytime submit button is clicked
+        iframe.current.srcdoc = html;
+
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
@@ -37,8 +41,29 @@ const App = () => {
             }
         });
 
-        setCode(result.outputFiles[0].text);
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
     };
+
+    const html = `
+        <html>
+            <head></head>
+            <body>
+                <div id="root"></div>
+                <script>
+                    window.addEventListener('message', (event) => {
+                        try{
+                            eval(event.data);
+                        } catch (err) {
+                            const root = document.querySelector('#root');
+                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+                            console.error(err);
+                        }
+                    }, false);
+                </script>
+            <body>
+        </html>
+    `;
 
     return (
         <div>
@@ -46,7 +71,8 @@ const App = () => {
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <pre>{code}</pre>
+            {/* <pre>{code}</pre> */}
+            <iframe ref={iframe} title="preview" sandbox="allow-scripts" srcDoc={html} />
         </div>
     );
 };
