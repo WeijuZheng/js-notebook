62ee2c5611a12a98c4ba9561e9719cce235334ac
weiju <weijuzheng1995@gmail.com>
2021-05-21 17:46:34 -0700

    removed dist directory

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var commander_1 = require("commander");
-var serve_1 = require("./commands/serve");
-// add the command handler to the program
-commander_1.program.addCommand(serve_1.serveCommand);
-commander_1.program.parse(process.argv);
