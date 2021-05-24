9f7475f944ad0d7ec50436f802b7c5a6ee28b1b2
weiju <weijuzheng1995@gmail.com>
2021-05-24 15:24:21 -0700

    added local-client as dependency, so that it can access the react app when running on client's machine; added routes to read and write the content to/from the file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
-export const serve = (port: number, filename: string, dir: string) => {
-    console.log('Server traffic on port', port);
-    console.log('saving/fetch cells from', filename);
-    console.log('that file is in dir', dir);
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
+import { createCellsRouter } from './routes/cells';
+
+export const serve = (port: number, filename: string, dir: string, useProxy: boolean) => {
+    const app = express();
+
+    // useProxy is true if it's on the development mode
+    if (useProxy) {
+        // use proxy to local Create React App server
+        app.use(createProxyMiddleware({
+            target: 'http://localhost:3000',
+            ws: true,
+            logLevel: 'silent'
+        }));
+    } else {
+        // use require.resolve to figure out the location of the file
+        const packagePath = require.resolve('local-client/build/index.html');
+        app.use(express.static(path.dirname(packagePath)));
+    }
+
+    app.use(createCellsRouter(filename, dir));
+
+    // return a promiss so that the cli can await this function
+    // to see if there is an error
+    return new Promise<void>((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject);
+    });
 };
