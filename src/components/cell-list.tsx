50387851c5c825bb910074a7ce1f260471640911
weiju <weijuzheng1995@gmail.com>
2021-05-24 17:35:33 -0700

    added fetch/save cells logic, added redux middleware to save cells

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,13 +1,21 @@
 import './cell-list.css';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
 import AddCell from './add-cell';
+import { useActions } from '../hooks/use-actions';
 
 const CellList: React.FC = () => {
     const cells = useTypedSelector(({ cells: { order, data } }) => {
         return order.map(id => data[id]);
     });
+
+    const { fetchCells } = useActions();
+
+    // fetch cells from the file when this component is created
+    useEffect(() => {
+        fetchCells();
+    }, []);
 
     const renderCells = cells.map(cell => (
         <Fragment key={cell.id}>
--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -22,6 +22,29 @@ const initialState: CellsState = {
 // using immer library to update the state
 const reducer = produce((state: CellsState, action: Action) => {
     switch (action.type) {
+        case ActionType.FETCH_CELLS: {
+            state.loading = true;
+            state.error = null;
+            break;
+        }
+        case ActionType.FETCH_CELLS_COMPLETE: {
+            state.loading = false;
+            state.order = action.payload.map(cell => cell.id);
+            state.data = action.payload.reduce((acc, cell) => {
+                acc[cell.id] = cell;
+                return acc;
+            }, {} as CellsState['data']);
+            break;
+        }
+        case ActionType.FETCH_CELLS_ERROR: {
+            state.loading = false;
+            state.error = action.payload;
+            break;
+        }
+        case ActionType.SAVE_CELLS_ERROR: {
+            state.error = action.payload;
+            break;
+        }
         case ActionType.UPDATE_CELL: {
             const { id, content } = action.payload;
             state.data[id].content = content;
